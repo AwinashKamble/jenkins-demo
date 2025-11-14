b6cbdbad9b88a397dd6881e7dc812dd1d9ed0d78
AwinashKamble <awinashkamble8@gmail.com>
2025-11-14 19:21:41 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"🔥 App Running Successfully on EC2 via Docker 🔥")
+        self.wfile.write(b"App Running Successfully on EC2 via Docker")
 
 if __name__ == '__main__':
     server = HTTPServer(('0.0.0.0', 5000), handler)
